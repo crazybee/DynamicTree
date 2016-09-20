bcabc77e11e1beed91a78c03f793d11006af2e81
crazybeeliuzhe <zheliu@outlook.com>
2016-09-20 10:32:41 +0200

    master commit

---
+++ TreeSite/Default.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <%-- jquery code to show loading panel at proper place--%>
+<script src="Scripts/jquery-1.10.2.js"></script>
+ <script language="javascript" type="text/javascript">
+        function placeUP() {
+            var mouseX;
+            var mouseY;
+            // below line for get mouse position
+            $(document).mousemove(function (e) {
+                mouseX = e.pageX;
+                mouseY = e.pageY;
+ 
+            });
+            // below line for show loading panel at proper place
+            $('#<%= TreeView1.ClientID%> a').click(function () {
+                $('#UP').css({'top': mouseY, 'left': mouseX});
+            });
+        }
+    </script>
+    
+
+</asp:Content>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h3>Populate Treeview Nodes Dynamically (On Demand)</h3>
+     <asp:TextBox ID="ValueText" runat="server" Text="" Width="50%"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Submit" runat="server" OnClick="Button_Click" Text="Find Node" /> 
+        <br />
+        <br />
+    <%-- UpdateProgress is used for show loading panel while loading child nodes --%>
+    <asp:UpdateProgress ID="UpdateProgress1" runat="server">
+        <ProgressTemplate>
+            <div id="UP" style="position:absolute;background-image:url('loading.gif'); background-repeat:no-repeat;
+                width:20px;">&nbsp;</div>
+        </ProgressTemplate>
+    </asp:UpdateProgress>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>       
+ 
+            <script type="text/javascript">
+                Sys.Application.add_load(placeUP);
+            </script>     
+            <%-- Here ExpandDepth="0" for eleminates the expansion of the added treenodes --%>
+            <asp:TreeView ID="TreeView1" runat="server" ExpandDepth="0" NodeIndent="15" PopulateNodesFromClient="false"  PopulateOnDemand="True" OnTreeNodePopulate="TreeView1_TreeNodePopulate">
+            <HoverNodeStyle Font-Underline="True" ForeColor="#6666AA" />
+             <NodeStyle Font-Names="Tahoma" Font-Size="8pt" ForeColor="Black" HorizontalPadding="2px"
+               NodeSpacing="0px" VerticalPadding="2px"></NodeStyle>
+             <ParentNodeStyle Font-Bold="False" />
+             <SelectedNodeStyle BackColor="#B5B5B5" Font-Underline="False" HorizontalPadding="0px" VerticalPadding="0px" />
+            </asp:TreeView>
+        </ContentTemplate>
+    </asp:UpdatePanel>  
+</asp:Content>
---
+++ TreeSite/Default.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    private List<TreeNode> _matchingNodes;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            PopulateCarTypes();
+        }
+    }
+
+    private void PopulateCarTypes()
+    {
+        List<VehicleType> allTypes = new List<VehicleType>();
+        using (MyDatabaseEntities dc = new MyDatabaseEntities())
+        {
+            allTypes = dc.VehicleTypes.ToList();
+        }
+        foreach (VehicleType c in allTypes)
+        {
+            TreeNode t = new TreeNode(
+                $"Node Name:{c.Name} " +
+                $"Node Id:{c.Id} " +
+                $"Created on:{c.TimeStamp} " +
+                $"Description{c.Description}",
+                c.Id.ToString());
+            t.PopulateOnDemand = true;
+            TreeView1.Nodes.Add(t);
+        }
+    }
+
+    protected void TreeView1_TreeNodePopulate(object sender, TreeNodeEventArgs e)
+    {
+        // This code is for populate Child nodes
+        TreeNode main = e.Node;
+        int carTypeId = Convert.ToInt32(main.Value);
+        List<VehicleSubType> subTypes = new List<VehicleSubType>();
+        using (MyDatabaseEntities dc = new MyDatabaseEntities())
+        {
+            subTypes = dc.VehicleSubTypes.Where(a => a.VehicleTypeId.Equals(carTypeId)).OrderBy(a => a.Name).ToList();
+        }
+        foreach (VehicleSubType s in subTypes)
+        {
+            TreeNode sub = new TreeNode($"Node Name:{s.Name} " +
+                                        $"Node Id:{s.Id} " +
+                                        $"Created on:{s.TimeStamp} " +
+                                        $"Description{s.Description}", 
+                                        s.Id.ToString());
+            main.ChildNodes.Add(sub);
+        }
+    }
+
+    protected void Button_Click(object sender, EventArgs e)
+    {
+
+        ProcessTreeView(TreeView1, ValueText.Text.ToString());
+        if (_matchingNodes.Count != 0)
+            foreach (var node in _matchingNodes)
+            {
+                node.Select();
+                node.Expand();
+                ValueText.Text = "Found";
+
+            }
+        else
+        {
+            ValueText.Text = "Not found";
+        }
+    }
+    private void ProcessTreeView(TreeView treeView, String FindText)
+    {
+        _matchingNodes = new List<TreeNode>();
+
+        // Process each node recursively.
+        foreach (TreeNode n in treeView.Nodes)
+        {
+            if (n.Text.Contains(FindText))
+                _matchingNodes.Add(n);
+            ProcessRecursive(n, FindText);
+        }
+
+    }
+
+    private void ProcessRecursive(TreeNode treeNode, String FindText)
+    {
+        // Process each node recursively.
+        foreach (TreeNode n in treeNode.ChildNodes)
+        {
+            if (n.Text.Contains(FindText))
+                _matchingNodes.Add(n);
+            ProcessRecursive(n, FindText);
+        }
+    }
+
+}
